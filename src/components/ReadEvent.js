7f197f365a3aaa9c9428d2328622d471b0d1dfae
LoukaOctave <louka.octave@student.odisee.be>
2021-01-13 01:11:49 +0100

    Added "Update", "Delete" & "Back" buttons

--- src/components/ReadEvent.js
+++ src/components/ReadEvent.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import firebase from "./Firebase.js";
 
 const ReadEvent = () => {
 
-    const [event, setEvent] = useState({fetched: false})
+    const [event, setEvent] = useState({fetched: false, found: false})
 
     useEffect(() => { // TODO: Fix this, because it keeps giving a warning
         fetchEvent();
@@ -16,6 +16,7 @@ const ReadEvent = () => {
         await response.get().then(function(doc) {
             if (doc.exists) {
               setEvent({
+                id: doc.id,
                 title: doc.data().title,
                 date: doc.data().date,
                 start: doc.data().start,
@@ -58,23 +59,30 @@ const ReadEvent = () => {
                 </div>
             </div>
             <div className={(event.fetched && event.found) ? "" : "hide"}>
-                    <ul className="collection with-header">
-                        <li className="collection-header"><h4>{event.title}</h4></li>
-                        <li className="collection-item"><div>Date<span className="secondary-content">{event.date}</span></div></li>
-                        <li className="collection-item"><div>Start<span className="secondary-content">{event.start}</span></div></li>
-                        <li className="collection-item"><div>End<span className="secondary-content">{event.end}</span></div></li>
-                        <li className="collection-item"><div>Type<span className="secondary-content">{event.type}</span></div></li>
-                        <li className="collection-item"><div>Description<span className="secondary-content">{event.description}</span></div></li>
-                    </ul>
+                <ul className="collection with-header">
+                    <li className="collection-header"><h4>{event.title}</h4></li>
+                    <li className="collection-item"><div>Date<span className="secondary-content">{event.date}</span></div></li>
+                    <li className="collection-item"><div>Start<span className="secondary-content">{event.start}</span></div></li>
+                    <li className="collection-item"><div>End<span className="secondary-content">{event.end}</span></div></li>
+                    <li className="collection-item"><div>Type<span className="secondary-content">{event.type}</span></div></li>
+                    <li className="collection-item"><div>Description<span className="secondary-content">{event.description}</span></div></li>
+                </ul>
+                <div>
+                    <Link to={`/updateEvent/${event.id}`} className="waves-effect waves-light btn"><i className="material-icons left">update</i>Update</Link>
+                    <a className="waves-effect waves-light btn"><i className="material-icons left">delete</i>Delete</a>
                 </div>
-                <div className={(event.fetched && !event.found) ? "" : "hide"}>
-                    <h4>404: Event not found</h4>
-                    <p>{event.errorMessage}</p>
-                </div>
-
-            
+            </div>
+            <div className={(event.fetched && !event.found) ? "" : "hide"}>
+                <h4>404: Event not found</h4>
+                <p>{event.errorMessage}</p>
+            </div>
+            <div>
+                <Link to="/" className="waves-effect waves-light btn"><i className="material-icons left">arrow_back_ios</i>Back</Link>
+            </div>
         </div>
     );
 };
 
 export default ReadEvent;
+
+//TODO: Make "Go Back" button actually go back, and not route to a predetermined path.
