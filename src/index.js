a3b873907b5584347a2990c13f7569b304d60f8d
LoukaOctave <louka.octave@student.odisee.be>
2021-01-13 01:06:10 +0100

    Added TODOS

--- src/index.js
+++ src/index.js
@@ -10,3 +10,17 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+//#region TO-DO
+
+  //TODO: Replace /src/logo.svg with image from https://pixabay.com/vectors/present-box-dole-favor-gift-150291/
+
+  //TODO: Replace dev Firebase JS SDK with individual components when deploying prod
+
+  //TODO: App: Write onClick function to toggle className="active" on <li/> elements to indicate which page is currently open.
+
+  //TODO: CreateEvent: Don't add "start" and "end" to newly created event if "allDay = true"
+
+  //TODO: ReadEvent: Make "Go Back" button actually go back, and not route to a predetermined path.
+
+//#endregion TO-DO
