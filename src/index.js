bc2c7b45b7e927c7e487edc140aaa46672518ebd
LoukaOctave <louka.octave@student.odisee.be>
2021-01-14 00:35:47 +0100

    Updated TODOs

--- src/index.js
+++ src/index.js
@@ -13,13 +13,23 @@ ReactDOM.render(
 
 //#region TO-DO
 
+  //TODO: Login
+  //TODO: Profile (with user URLs)
+  //TODO: Settings
+  //TODO: Make UI more attractive
+  //TODO: Indicate when app is offline
+  //TODO: Wishlists
+  //TODO: Make use of an API that retrieves gift suggestions
+  //TODO: Make use of an API that retrieves holiday data
+
   //TODO: Replace /src/logo.svg with image from https://pixabay.com/vectors/present-box-dole-favor-gift-150291/
 
   //TODO: Replace dev Firebase JS SDK with individual components when deploying prod
 
-  //TODO: App: Write onClick function to toggle className="active" on <li/> elements to indicate which page is currently open.
+  //TODO: Home: Add a calendar to select a date from
 
-  //TODO: CreateEvent: Don't add "start" and "end" to newly created event if "allDay = true"
+  //TODO: DateInfoCard: Convert date to human language
+  //TODO: DateInfoCard: Fix issue that prevents the display of multiple links in the card content
 
   //TODO: ReadEvent: Make "Go Back" button actually go back, and not route to a predetermined path.
 
